92cfa1ebca7f7e38f2d3b9d02ea7d86bbf4a3984
datluu <tad.louis2810@gmail.com>
2024-08-05 11:04:39 +1000

    redo webpack

--- exporter-chrome-extesion/src/scripts/supabaseClient.js
+++ exporter-chrome-extesion/src/scripts/supabaseClient.js
@@ -2,3 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
+
+const { data: user, error } = await supabase.from('todos').select('*')
